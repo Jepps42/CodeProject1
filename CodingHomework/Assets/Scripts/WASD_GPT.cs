a5244e52cc255cdab3c67830a8b68c3610f390d3
Jepps42 <63672521+Jepps42@users.noreply.github.com>
2024-09-12 17:41:40 -0400

    ChatGPT WASD added

--- CodingHomework/Assets/Scripts/WASD_GPT.cs
+++ CodingHomework/Assets/Scripts/WASD_GPT.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class WASD_GPT : MonoBehaviour
 {
+    //Variables
+    public float movespeed = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Find rigidbody on this script
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Get input from WASD keys
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        movement = new Vector2(moveX, moveY).normalized;
+    }
+
+    //Fixed update is called on a fixed value of frames
+    private void FixedUpdate()
+    {
+        //Move the player
+        rb.MovePosition(rb.position + movement * movespeed * Time.fixedDeltaTime);
     }
 }
