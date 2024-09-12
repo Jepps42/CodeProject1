a14dc19a614ae70eb7954024873e7ba465a08517
Jepps42 <63672521+Jepps42@users.noreply.github.com>
2024-09-12 17:09:49 -0400

    Added player 2 with different controller

--- CodingHomework/Assets/Scripts/WASD_TRAN.cs
+++ CodingHomework/Assets/Scripts/WASD_TRAN.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class WASD_TRAN : MonoBehaviour
 {
+    //Variables 
+
+    public float speed = 3f;
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,36 @@ public class WASD_TRAN : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Find the position of the object every frame and make a vector
+        Vector2 pos = transform.position;
+
+        //If I press right arrow, move the postion to the right
+        //If is called when input of right arrow is down
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            pos.x += speed * Time.deltaTime;
+
+        }
+
+        //If I press A, move the position to the left
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+
+        //If I press up arrow, move the position up
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            pos.y += speed * Time.deltaTime;
+        }
         
+        //If I press down arrow, move the position down
+        if ((Input.GetKey(KeyCode.DownArrow)))
+        {
+            pos.y -= speed * Time.deltaTime;
+        }
+
+        //Transforms postion, needs to be the last line in update function
+        transform.position = pos;
     }
 }
