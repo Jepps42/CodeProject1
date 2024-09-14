12fdf812e2211aa1daaf39e3c33a1818b77b04c9
Jepps42 <63672521+Jepps42@users.noreply.github.com>
2024-09-13 23:17:02 -0400

    Declared variables

---
+++ CodingHomework/Assets/Scripts/WASD_JE.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WASD_JE : MonoBehaviour
+{
+    //Variables
+    public float movespeed = 7f;
+
+    private Rigidbody2D rb;
+
+    private Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
